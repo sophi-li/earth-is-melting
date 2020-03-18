e5cbe84691e7b3311f4addeac6a043b93b07ac98
Sophia Li <sophiali124@gmail.com>
2020-03-17 18:18:36 -0700

    update nasa api

--- TimerTrigger/index.js
+++ TimerTrigger/index.js
@@ -1,58 +1,8 @@
-// const accountSid = process.env.TWILIO_SID;
-// const authToken = process.env.TWILIO_TOKEN;
-// const client = require("twilio")(accountSid, authToken);
-// const nasaAPI = process.env.NASA_API;
-// const fetch = require("node-fetch");
-
-// module.exports = async function(context, myTimer) {
-//   var timeStamp = new Date().toISOString();
-
-//   if (myTimer.isPastDue) {
-//     context.log("JavaScript is running late!");
-//   }
-//   context.log("JavaScript timer trigger function ran!", timeStamp);
-
-//   //   NASA Request
-//   let url = "https://api.nasa.gov/planetary/apod?api_key=" + nasaAPI;
-
-//   // Set up title and picture for text message
-//   const fetchNasa = await fetch(url);
-//   const response = await fetchNasa.json();
-//   const data = {
-//     picture: await response.url,
-//     title: await response.title
-//   };
-
-//   // Text message set up
-//   client.messages
-//     .create({
-//       from: process.env.SENDER_NUMBER,
-//       body: `Daily earth update: ${data.title}`,
-//       mediaUrl: [`${data.picture}`],
-//       to: process.env.RECIPIENT_NUMBER
-//     })
-//     .then(message => {
-//       context.log("Message sent");
-//       context.res = {
-//         // status: 200, /* Defaults to 200 */
-//         body: "Text successfully sent"
-//       };
-//       context.log("JavaScript timer trigger function ran!", timeStamp);
-//       context.done();
-//     })
-//     .catch(err => {
-//       context.log.error("Twilio Error: " + err.message + " -- " + err.code);
-//       context.res = {
-//         status: 500,
-//         body: `Twilio Error Message: ${err.message}\nTwilio Error code: ${err.code}`
-//       };
-//       context.done();
-//     });
-// };
-
+// Save Twilio keys
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
 const client = require("twilio")(accountSid, authToken);
+// Save NASA API key
 const nasaAPI = process.env.NASA_API;
 const fetch = require("node-fetch");
 
@@ -64,31 +14,38 @@ module.exports = async function(context, myTimer) {
   }
   context.log("JavaScript timer trigger function ran!", timeStamp);
 
-  //   NASA Request
-  let url = "https://api.nasa.gov/EPIC/api/natural/images?api_key=" + nasaAPI;
+  let url = `https://api.nasa.gov/EPIC/api/natural/images?api_key=${nasaAPI}`;
 
-  // Set up title and picture for text message
+  // Set up picture for text message
   const fetchNasa = await fetch(url);
   const response = await fetchNasa.json();
   const data = {
     imageID: await response[0].image,
-    caption: await response[0].caption,
     date: await response[0].date
   };
 
+  // Set up date for text message
   const year = new Date(data.date).getFullYear();
   let month = new Date(data.date).getMonth() + 1;
+  // Format single digit dates to become double digits for compatibility with NASA's image URL
   if (month < 10) {
-    month = "0" + month;
+    month = `0${month}`;
   }
-  const day = new Date(data.date).getDate();
+  let day = new Date(data.date).getDate();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  const currentImageDate = `${year}-${month}-${day}`;
+
+  // Set up image URL for text message
   const imgURL = `https://epic.gsfc.nasa.gov/archive/natural/${year}/${month}/${day}/png/${data.imageID}.png`;
 
   // Text message set up
   client.messages
     .create({
       from: process.env.SENDER_NUMBER,
-      body: `Daily earth update: ${data.caption}`,
+      body: `How do I look? Here's a photo of me from ${currentImageDate}`,
+      // 'mediaURL' opens and formats the URL so the image on the receipient's device
       mediaUrl: [`${imgURL}`],
       to: process.env.RECIPIENT_NUMBER
     })
